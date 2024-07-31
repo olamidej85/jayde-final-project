7d03345f7b203eabf67019f54cb3bd4e3760c4c5
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-07-31 06:54:54 -0700

    Added the github repo URL

--- my-final-project/src/App.jsx
+++ my-final-project/src/App.jsx
@@ -4,5 +4,11 @@ import "./App.css";
 export default function App() {
   return <div className="App"> 
   <h1>Weather App</h1>
+  <footer> 
+    This project was coded by Jayde and is {""}
+    <a href="https://github.com/olamidej85/jayde-final-project" target="_blank">
+    open-sourced on Github
+      </a>
+  </footer>
     </div>;
 }
