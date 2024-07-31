ab5d650a48cfe23a26c50858f794afaca1966a79
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-07-31 03:53:55 -0700

    added a name

--- my-final-project/src/App.jsx
+++ my-final-project/src/App.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return <div className="App">Weather Application </div>;
 }
