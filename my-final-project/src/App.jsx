384a40c9bec0d27da011c8f1e72b7d0d46a6ce2c
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-07-31 09:45:44 -0700

    Added bootstrap

--- my-final-project/src/App.jsx
+++ my-final-project/src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 
 export default function App() {
   return <div className="App"> 
+  <div className="container">
   <h1>Weather App</h1>
+  <button className="btn btn-primary">hello</button>
   <footer> 
     This project was coded by Jayde and is {""}
     <a href="https://github.com/olamidej85/jayde-final-project" target="_blank">
     open-sourced on Github
       </a>
   </footer>
+  </div>
     </div>;
 }
