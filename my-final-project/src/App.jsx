ab6f7447496b3f92e6501b46883f7adf6c695b88
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-07-31 04:01:14 -0700

    added an hh1

--- my-final-project/src/App.jsx
+++ my-final-project/src/App.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application </div>;
+  return <div className="App"> 
+  <h1>Weather App</h1>  </div>;
 }
