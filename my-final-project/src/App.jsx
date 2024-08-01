931268061daa77afd0a2957a6aa13650546a8e89
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-08-01 03:01:10 -0700

    Added weather.jsx

--- my-final-project/src/App.jsx
+++ my-final-project/src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
+import Weather from './weather.jsx';
 
 export default function App() {
   return <div className="App"> 
   <div className="container">
-  <h1>Weather App</h1>
-  <button className="btn btn-primary">hello</button>
+  
+  <Weather />
   <footer> 
     This project was coded by Jayde and is {""}
     <a href="https://github.com/olamidej85/jayde-final-project" target="_blank">
-    open-sourced on Github
+    open-sourced on Github 
       </a>
   </footer>
   </div>
---
+++ my-final-project/src/weather.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Weather.css";
+
+
+export default function weather() {
+    return <div className="weather">
+        <h1>Nigeria</h1>
+        <ul>
+            <li>Thursday 10:52</li>
+            <li>Cloudy</li>
+        </ul>
+            <h1>23°C</h1>
+        
+    </div>;
+}
